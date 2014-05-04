5f42f8dabb2e4c584b0c4ffc01200a6251c9693e
Shawn Miller <shawnrm90@gmail.com>
2014-05-04 04:04:03 -0700

    things

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
